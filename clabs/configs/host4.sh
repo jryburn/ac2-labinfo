5a59043f62f38a7b45bda2ca16d0b1161dd11259
Justin Ryburn <justin@ryburn.org>
2024-11-07 15:23:52 -0600

    Fix host configs

--- clabs/configs/host4.sh
+++ clabs/configs/host4.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 # Update the package list and upgrade existing packages
-sudo apt update -y && sudo apt upgrade -y
+apt update -y && apt upgrade -y
 
 # Install vlan
-sudo apt install -y vlan
+apt install -y vlan
 
 # Install iproute2
-sudo apt install -y iproute2
+apt install -y iproute2
 
 # Install ping
-sudo apt install -y iputils-ping
+apt install -y iputils-ping
 
 # Install net-tools so arp command works
-sudo apt install -y net-tools
+apt install -y net-tools
 
 # Install tcpdump
-sudo apt install -y tcpdump
+apt install -y tcpdump
 
 # Creating a bonded Ethernet interface (bond0)
-sudo ip link add bond0 type bond mode 802.3ad
-sudo ip link set eth1 down
-sudo ip link set eth1 master bond0
-sudo ip link set eth2 down
-sudo ip link set eth2 master bond0
-sudo ip link set eth3 down
-sudo ip link set eth3 master bond0
-sudo ip link set eth4 down
-sudo ip link set eth4 master bond0
-sudo ip link set bond0 up
-sudo ip link add link bond0 name bond0.78 type vlan id 78
-sudo ip link set bond0.78 up
-sudo ip addr add 10.78.78.78/24 dev bond0.78
+ip link add bond0 type bond mode 802.3ad
+ip link set eth1 down
+ip link set eth1 master bond0
+ip link set eth2 down
+ip link set eth2 master bond0
+ip link set eth3 down
+ip link set eth3 master bond0
+ip link set eth4 down
+ip link set eth4 master bond0
+ip link set bond0 up
+ip link add link bond0 name bond0.78 type vlan id 78
+ip link set bond0.78 up
+ip addr add 10.78.78.78/24 dev bond0.78
