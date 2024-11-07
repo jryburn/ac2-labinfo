e31b2667bc68d297434e0c88ee4ff258e414034f
Justin Ryburn <justin@ryburn.org>
2024-11-06 23:14:55 -0600

    Add files via upload

---
+++ clabs/configs/host2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Update the package list and upgrade existing packages
+sudo apt update -y && sudo apt upgrade -y
+
+# Install iproute2
+sudo apt install -y iproute2
+
+# Install ping
+sudo apt install iputils-ping
+
+# Install net-tools so arp command works
+sudo apt install net-tools
+
+# Creating a bonded Ethernet interface (bond0)
+sudo ip link add bond0 type bond mode 802.3ad
+sudo ip link set eth1 down
+sudo ip link set eth1 master bond0
+sudo ip link set eth2 down
+sudo ip link set eth2 master bond0
+sudo ip link set eth3 down
+sudo ip link set eth3 master bond0
+sudo ip link set eth4 down
+sudo ip link set eth4 master bond0
+sudo ip link set bond0 up
+sudo ip link add link bond0 name bond0.34 type vlan id 34
+sudo ip link set bond0.34 up
+sudo ip addr add 10.34.34.34/24 dev bond0.34
